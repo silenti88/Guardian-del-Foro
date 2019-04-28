878cc767106842315891d3a1a0e6b0bd154f3522
Dominic Humber <45247402+silenti88@users.noreply.github.com>
2019-04-28 16:23:41 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+let date = new Date();
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on("disconnect", () => {
+    console.log(`You have been disconnected`);
+});
+
+client.on("error", () => {
+    console.error();
+});
+
+client.on("reconnecting", () => {
+    console.log("You have been reconnected");
+});
+
+client.login();
+
+client.setInterval(function channelStatus(){
+    date = new Date();
+    let nightChannel = client.guilds.get('560406112956973063').roles.get('560414885012439042');
+
+    if(date.getHours() === 20 && date.getMinutes() === 0){
+        client.channels.get("560406616839553034").overwritePermissions(nightChannel,{
+            SEND_MESSAGES: true
+        }).catch(console.error())
+    }else if(date.getHours() === 24 && date.getMinutes() === 0){
+
+        client.channels.get("560406616839553034").overwritePermissions(nightChannel,{
+            SEND_MESSAGES: false
+        }).catch(console.error())
+    }
+},1000*50);
